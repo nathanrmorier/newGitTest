8b748d44d4bbd5e6a280c3e861e3ecf0a74df123
nathanrmorier <31260095+nathanrmorier@users.noreply.github.com>
2017-08-22 22:30:48 -0300

    look at that! I added a new file :)

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+		<ul>
+			<li>Milk</li>
+			<li>Eggs</li>
+			<li>Cheese</li>
+			<li>Bread</li>
+			<li>Bacon</li>
+			<?php echo ‘<li>A little php for ya?</li>’; ?>
+		</ul>
+	</body>
+</html>
